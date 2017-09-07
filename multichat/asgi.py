0834cabb6e7b6145ff51c37ce6072be7c568671b
JuhyunPark-Yonsei <juhyun1849@gmail.com>
2017-09-07 16:06:37 +0900

    asgi, wsgi 수정

--- multichat/asgi.py
+++ multichat/asgi.py
@@ -7,5 +7,5 @@ ASGI applications at "multichat.asgi:channel_layer" as their channel layer.
 
 import os
 from channels.asgi import get_channel_layer
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.prod")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "multichat.settings.prod")
 channel_layer = get_channel_layer()
