724aad09f10b0339ebf362d393d8be3a554ac30d
JuhyunPark-Yonsei <juhyun1849@gmail.com>
2017-09-08 00:56:27 +0900

    배포환경 수정(2)

--- multichat/settings/prod.py
+++ multichat/settings/prod.py
@@ -4,15 +4,8 @@ import dj_database_url
 DEBUG=False
 SECRET_KEY='imasecret'
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'),
+                                      conn_max_age=500)
 }
-
-
-#DATABASES = {
-#    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'),
-#                                      conn_max_age=500)
-#}
