1cecf4944e000f7d66f7b6879f0b69dd6cdae112
JuhyunPark-Yonsei <juhyun1849@gmail.com>
2017-09-07 10:29:44 +0900

    실행환경(개발, 배포)에 따른 requirements.txt 및 setting 파일 분리

--- multichat/settings/prod.py
+++ multichat/settings/prod.py
@@ -1,3 +1,9 @@
 from .common import *
+import dj_database_url
 
 DEBUG=False
+SECRET_KEY=os.environ.get('SECRET_KEY')
+DATABASES = {
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'),
+                                      conn_max_age=500)
+}
