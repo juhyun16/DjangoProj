7e35b04f1f1a22efaff3619fe59af68cef502a1a
JuhyunPark-Yonsei <juhyun1849@gmail.com>
2017-09-07 12:00:34 +0900

    배포환경으로 변경

--- multichat/settings/prod.py
+++ multichat/settings/prod.py
@@ -2,7 +2,7 @@ from .common import *
 import dj_database_url
 
 DEBUG=False
-SECRET_KEY=os.environ.get('SECRET_KEY')
+SECRET_KEY='imasecret'
 DATABASES = {
     'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'),
                                       conn_max_age=500)
