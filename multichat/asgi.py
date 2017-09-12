5fff5eccfe04f0abe5818a85ee743ed6419ea5d4
JuhyunPark-Yonsei <juhyun1849@gmail.com>
2017-09-13 00:16:04 +0900

    procfile 수정. runserver로 바꾸었다.

--- multichat/asgi.py
+++ multichat/asgi.py
@@ -7,6 +7,6 @@ ASGI applications at "multichat.asgi:channel_layer" as their channel layer.
 
 import os
 from channels.asgi import get_channel_layer
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "multichat.prod")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "multichat.dev")
 
 channel_layer = get_channel_layer()
--- multichat/wsgi.py
+++ multichat/wsgi.py
@@ -9,7 +9,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "multichat.prod")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "multichat.dev")
 
 application = get_wsgi_application()
 
